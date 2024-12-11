c53c2f79f7a147ca68cfc78187a1fad707fe1a2e
AkhtarZaman12 <akhtar.tarar12@gmail.com>
2024-12-11 02:16:13 -0800

    chores: readme

--- src/app/assessment/layout.tsx
+++ src/app/assessment/layout.tsx
@@ -4,7 +4,7 @@ export default function AssessmentLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-gradient-to-b from-[#1C1C1E] to-[#000000] text-white">
+    <div className="min-h-screen bg-gradient-to-b from-[#1C1C1E] to-[#000000] text-white py-11">
       {children}
     </div>
   );
